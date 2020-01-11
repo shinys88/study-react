a718f15d20899d2c7d1b6227602c74e55162e013
ShinYS88 <sinyusu129@gmail.com>
2020-01-11 23:57:51 +0900

    컴포넌트 테스트

---
+++ src/App2.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MyCompoent from "./component/MyComponent";
+
+const App2 = () => {
+	return <MyCompoent />;
+};
+
+export default App2;
---
+++ src/component/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// 함수형 컴포넌트
+const MyComponent = () => {
+	return <div>나의 새롭고 멋진 컴포넌트</div>;
+};
+
+// const MyCompoent2 = props => {
+// 	return <div>컴포넌트 테스트, 이름은 {props.name}입니다.</div>
+// };
+
+export default MyComponent;
+
